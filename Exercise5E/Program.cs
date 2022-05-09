8c87a471340a900c83b27fae7779714f2776fc12
ac149483 <ac149483@8CG8163L1Y>
2022-05-09 12:57:56 +1200

    Add project files.

---
+++ Exercise5E/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("This program will show degrees in Celsius converted to Farenheit\n");
+
+Console.Write("Enter the degree value in celsius: ");
+double c = Convert.ToInt32(Console.ReadLine());
+
+while(c < -271.15)
+{
+    Console.Write("\nPlease enter a value above the absolute zero (271.15): ");
+    c = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("=======  \t ==========");
+Console.WriteLine("Celsius  \t Fahrenheit");
+Console.WriteLine("=======  \t ==========");
+
+Console.WriteLine("  " + c + "\t\t   " + CelsiustoFarenheit(c));
+
+
+static double  CelsiustoFarenheit(double celsius)
+{
+   return celsius * 9.0 / 5.0 + 32;
+    
+}
+
+
+
+
+
+
+
